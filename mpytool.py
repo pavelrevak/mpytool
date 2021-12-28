4e8237fab6f441bc18c96d93e21ebcaf258b32dd
Pavel Revak <pavel.revak@gmail.com>
2021-12-28 10:11:37 +0100

    divided into sub classes, some improvements and error handling

--- mpytool.py
+++ mpytool.py
@@ -1,3 +1,5 @@
+"""MicroPython tool"""
+
 import sys
 import time
 import argparse as _argparse
@@ -24,34 +26,43 @@ class CmdError(Exception):
             res += f'Error:\n  {self._error}'
         return res
 
+    @property
+    def cmd(self):
+        return self._cmd
 
-class MpyTool():
-    def __init__(self, loglevel=3, **serial_config):
-        self._loglevel = loglevel
+    @property
+    def result(self):
+        return self._result
+
+    @property
+    def error(self):
+        return self._error
+
+
+class FileNotFound(Exception):
+    """File not found"""
+    def __init__(self, file_name):
+        self._file_name = file_name
+        super().__init__(self.__str__())
+
+    def __str__(self):
+        return f"File '{self._file_name}' was not found"
+
+
+class DirNotFound(FileNotFound):
+    """Folder not found"""
+    def __str__(self):
+        return f"Dir '{self._file_name}' was not found"
+
+
+class SerialConn():
+    def __init__(self, log=None, **serial_config):
+        self._log = log
         self._serial = _serial.Serial(**serial_config)
-        self._imported = []
-
-    def log(self, msg):
-        print(msg, file=sys.stderr)
-
-    def log_err(self, msg):
-        if self._loglevel >= 1:
-            self.log(f"\033[1;31m{msg}\033[0m")
-
-    def log_warn(self, msg):
-        if self._loglevel >= 2:
-            self.log(f"\033[1;33m{msg}\033[0m")
-
-    def log_info(self, msg):
-        if self._loglevel >= 3:
-            self.log(f"\033[1;35m{msg}\033[0m")
-
-    def log_dbg(self, msg):
-        if self._loglevel >= 4:
-            self.log(f"\033[1;34m{msg}\033[0m")
 
     def write(self, data):
-        self.log_dbg(f"wr: {data}")
+        if self._log:
+            self._log.debug(f"wr: {data}")
         self._serial.write(data)
 
     def read_until(self, end, timeout=1):
@@ -64,94 +75,185 @@ class MpyTool():
                 data += self._serial.read()
             elif max_time < time.time():
                 if data:
-                    self.log_warn("tout: {data}")
                     raise Timeout(f"During timeout received: {data}")
-                self.log_warn("rd: timeout")
                 raise Timeout("No data received")
             else:
                 time.sleep(.01)
-        self.log_dbg(f"rd: {data}")
+        if self._log:
+            self._log.debug(f"rd: {data}")
         data = data.rstrip(end)
         return data
 
+    def read_line(self, timeout):
+        if timeout:
+            line = self.read_until(b'\n', timeout)
+        else:
+            line = self._serial.read_line()
+        return line.strip(b'\r')
+
+
+class MpyComm():
+    def __init__(self, conn, log=None):
+        self._conn = conn
+        self._log = log
+        self._repl_mode = None
+
+    @property
+    def conn(self):
+        return self._conn
+
     def enter_raw_repl(self):
-        self.log_info('ENTER RAW REPL')
-        # stop current operations (in case is app running)
-        self.write(b'\x03')
+        if self._repl_mode is True:
+            return
+        if self._log:
+            self._log.info('ENTER RAW REPL')
+        # stop current operations (in case if app running)
+        self._conn.write(b'\x03')
         try:
             # wait for prompt
-            self.read_until(b'\r\n>>> ', timeout=.2)
+            self._conn.read_until(b'\r\n>>> ', timeout=.2)
         except Timeout:
             # probably is in RAW repl
+            self._log.warning("Timeout while stopping program")
             self.exit_raw_repl()
         # enter raw repl
-        self.write(b'\x01')
-        self.read_until(b'\r\n>')
+        self._conn.write(b'\x01')
+        self._conn.read_until(b'\r\n>')
+        self._repl_mode = True
 
     def exit_raw_repl(self):
-        self.log_info('EXIT RAW REPL')
-        self.write(b'\x02')
-        self.read_until(b'\r\n>>> ')
+        if self._repl_mode is False:
+            return
+        if self._log:
+            self._log.info('EXIT RAW REPL')
+        self._conn.write(b'\x02')
+        self._conn.read_until(b'\r\n>>> ')
+        self._repl_mode = False
 
-    def run_command(self, cmd):
+    def soft_reset(self):
+        if self._repl_mode is True:
+            self.exit_raw_repl()
+        if self._log:
+            self._log.info('SOFT RESET')
+        self._conn.write(b'\x04')
+        self._repl_mode = None
+
+    def exec(self, command, timeout=1):
+        """Execute command
+
+        Arguments:
+            command: command to execute
+            timeout: maximum waiting time for result
+
+        Returns:
+            command STDOUT result
+
+        Raises:
+            CmdError when command return error
+        """
         # wait for prompt
-        self.log_info(f"CMD: {cmd}")
-        self.write(bytes(cmd, 'utf-8'))
-        self.write(b'\x04')
-        self.read_until(b'OK')
-        result = self.read_until(b'\x04')
+        if self._log:
+            self._log.info(f"CMD: {command}")
+        self._conn.write(bytes(command, 'utf-8'))
+        self._conn.write(b'\x04')
+        self._conn.read_until(b'OK')
+        result = self._conn.read_until(b'\x04', timeout)
         if result:
-            self.log_info(f'RES: {result}')
-        err = self.read_until(b'\x04>')
+            if self._log:
+                self._log.info(f'RES: {result}')
+        err = self._conn.read_until(b'\x04>', timeout)
         if err:
-            raise CmdError(cmd, result, err)
+            raise CmdError(command, result, err)
         return result
+
+
+class Mpy():
+    def __init__(self, conn, chunk=256, log=None):
+        self._conn = conn
+        self._chunk = chunk
+        self._log = log
+        self._mpy_comm = MpyComm(conn, log=log)
+        self._imported = []
+
+    @property
+    def conn(self):
+        return self._conn
+
+    @property
+    def comm(self):
+        return self._mpy_comm
 
     def import_module(self, module):
         if module not in self._imported:
-            self.run_command(f'import {module}')
+            self._mpy_comm.exec(f'import {module}')
             self._imported.append(module)
 
+    def ls(self, dir_name):
+        self.import_module('os')
+        try:
+            result = self._mpy_comm.exec(
+                f"print(os.listdir('{dir_name}'))")
+        except CmdError as err:
+            raise DirNotFound(dir_name) from err
+        return eval(result)
+
+    def get(self, file_name):
+        result = self._mpy_comm.exec(
+            f"f = open('{file_name}', 'rb')")
+        data = b''
+        while True:
+            result = self._mpy_comm.exec(
+                f"print(f.read({self._chunk}))")
+            result_eval = eval(result)
+            if not result_eval:
+                break
+            data += result_eval
+        self._mpy_comm.exec(f"f.close()")
+        return data
+
+
+class MpyTool():
+    def __init__(self, conn, log=None, verbose=0):
+        self._conn = conn
+        self._log = log
+        self._mpy = Mpy(conn, chunk=4096, log=log)
+
     def ls(self, *dir_names):
-        first = True
         for dir_name in dir_names:
             if not dir_name.startswith('/'):
                 dir_name = '/' + dir_name
             if not dir_name.endswith('/'):
                 dir_name = dir_name + '/'
             if len(dir_names) > 1:
-                if not first:
-                    print()
                 print(f"{dir_name}:")
-            self.import_module('os')
-            result = self.run_command(f"print(os.listdir('{dir_name}'))")
-            result_eval = eval(result)
-            for i in result_eval:
-                print(f"{i}")
-            first = False
+            try:
+                result = self._mpy.ls(dir_name)
+            except FileNotFound as err:
+                self._log.error(err)
+            else:
+                for i in result:
+                    print(f"{i}")
+            if len(dir_names) > 1:
+                print()
 
     def get(self, *file_names):
-        first = True
         for file_name in file_names:
             if not file_name.startswith('/'):
                 file_name = '/' + file_name
-            if len(file_names) > 1:
-                if not first:
-                    print()
-                print(f"{file_name}:")
-            result = self.run_command(f"f = open('{file_name}', 'rb')")
-            data = b''
-            while True:
-                result = self.run_command(f"print(f.read(256))")
-                result_eval = eval(result)
-                if not result_eval:
-                    break
-                data += result_eval
-            result = self.run_command(f"f.close()")
+            data = self._mpy.get(file_name)
             print(data.decode('utf-8'))
-            first = False
+
+    def dump_log(self):
+        try:
+            while True:
+                line = self._conn.read_line()
+                line = line.decode('utf-8')
+                print(line)
+        except KeyboardInterrupt:
+            return
 
     def process_commands(self, commands):
+        self._mpy.comm.enter_raw_repl()
         try:
             while commands:
                 command = commands.pop(0)
@@ -165,20 +267,53 @@ class MpyTool():
                         self.get(*commands)
                         break
                     self.ls('/')
+                elif command == 'reset':
+                    self._mpy.comm.soft_reset()
+                elif command == 'dump_log':
+                    self.dump_log()
         except CmdError as err:
-            self.log_err(err)
+            print(err)
+        self._mpy.comm.exit_raw_repl()
+
+
+class SimpleColorLogger():
+    def __init__(self, loglevel=0):
+        self._loglevel = loglevel
+
+    def log(self, msg):
+        print(msg, file=sys.stderr)
+
+    def error(self, msg):
+        if self._loglevel >= 1:
+            self.log(f"\033[1;31m{msg}\033[0m")
+
+    def warning(self, msg):
+        if self._loglevel >= 2:
+            self.log(f"\033[1;33m{msg}\033[0m")
+
+    def info(self, msg):
+        if self._loglevel >= 3:
+            self.log(f"\033[1;35m{msg}\033[0m")
+
+    def debug(self, msg):
+        if self._loglevel >= 4:
+            self.log(f"\033[1;34m{msg}\033[0m")
 
 
 def main():
     parser = _argparse.ArgumentParser()
     parser.add_argument('-p', '--port', required=True, help="serial port")
-    parser.add_argument('-v', '--verbose', default=0, action='count', help='increase verbosity')
+    parser.add_argument(
+        '-d', '--debug', default=0, action='count', help='set debug level')
+    parser.add_argument(
+        '-v', '--verbose', default=0, action='count', help='verbose output')
     parser.add_argument('commands', nargs='*', help='commands')
     args = parser.parse_args()
-    mpy = MpyTool(port=args.port, baudrate=115200, loglevel=args.verbose + 1)
-    mpy.enter_raw_repl()
-    mpy.process_commands(args.commands)
-    mpy.exit_raw_repl()
+
+    log = SimpleColorLogger(args.debug + 1)
+    serial_conn = SerialConn(port=args.port, baudrate=115200, log=log)
+    mpy_tool = MpyTool(serial_conn, log=log, verbose=args.verbose)
+    mpy_tool.process_commands(args.commands)
 
 
 if __name__ == '__main__':
